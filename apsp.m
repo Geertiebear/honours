49ae7ed85d6e111a0e257255ddaf372eded11326
Geert Custers <geert.aj.custers@gmail.com>
2021-06-02 08:59:28 +0200

    apsp: add working version

--- apsp.m
+++ apsp.m
@@ -1,18 +1,22 @@
 clear variables;
 close all;
 
-A = [   0 3 0 0 1; 
-        3 0 1 1 0;
-        0 1 0 0 0;
-        0 1 0 0 2;
-        1 0 0 2 0
+A = [   0 3 inf inf 1; 
+        3 0 1 1 inf;
+        inf 1 0 inf inf;
+        inf 1 inf 0 2;
+        1 inf inf 2 0
     ];
-
+G = graph(A);
+plot(G);
 D = A;
 
-for i = 1:5
-    x = (D(:, i) + D(i, :));
-    disp(x);
-    m = min(D(i), (D(:, i) + D(i, :)));
-    D = D + m;
+for k = 1:5
+    for i = 1:5
+        for j = 1:5
+            if D(i, j) > D(i, k) + D(k, j)
+                D(i, j) = D(i, k) + D(k, j);
+            end
+        end
+    end
 end
