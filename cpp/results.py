77a29f8933d050f5155ab1ba2072c9ed3f80e584
Geert Custers <geert.aj.custers@gmail.com>
2022-04-30 14:13:19 +0200

    results.py: add energy calculation

--- cpp/results.py
+++ cpp/results.py
@@ -4,8 +4,12 @@ from parse import *
 read_time = 10
 write_time = 100
 adc_latency = 1
+read_energy = 40 * 10**-15
+write_energy = 20 * 10**-12
+adc_energy = 2 * 10**-12
 
 total_time = 0
+total_energy = 0
 
 file = sys.argv[1]
 with open(file, "r") as results:
@@ -19,10 +23,13 @@ with open(file, "r") as results:
         elif command == "read":
             [adc_acts, row_reads, row_writes, inputs] = parse(" {:d},{:d},{:d},{:d}\n", parsed[1])
             total_time += (row_reads * read_time + adc_acts * adc_latency) * inputs
+            total_energy += (row_reads * read_energy + adc_acts * adc_energy) * inputs
         elif command == "write":
             [adc_acts, row_reads, row_writes, inputs] = parse(" {:d},{:d},{:d},{:d}\n", parsed[1])
             total_time += (row_writes * write_time + adc_acts * adc_latency)
+            total_energy += (row_writes * write_energy + adc_acts * adc_energy) * inputs
         else:
             print("Unknown command " + command)
 
-print("Total time: " + str(total_time))
+print("Total time: " + str(total_time / 10**9) + "s")
+print("Total energy: " + str(total_energy) + "J")
